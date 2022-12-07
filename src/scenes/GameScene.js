788161396647c17dc96cab51cead561d11cfec46
HENRY VERISSIMO <verissimohenry04@gmail.com>
2022-12-07 18:28:29 +0100

    Modif main.js and gamescene files

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,14 +2,14 @@ import Phaser from 'phaser'
 
 export default class GameScene extends Phaser.Scene
 {
-    constructor()
-    {
-        super('game-scene')
-    }
-    
-    preload()
-    {
-        this.load.image('sky', 'assets/sky.png')
+	constructor()
+	{
+		super('game-scene')
+	}
+
+	preload()
+	{
+		this.load.image('sky', 'assets/sky.png')
 		this.load.image('ground', 'assets/platform.png')
 		this.load.image('star', 'assets/star.png')
 		this.load.image('bomb', 'assets/bomb.png')
@@ -18,12 +18,11 @@ export default class GameScene extends Phaser.Scene
 			'assets/dude.png',
 			{ frameWidth: 32, frameHeight: 48 }
 		)
+	}
 
-    }
-
-    create()
-    {
-        this.add.image(400, 300, 'sky')
+	create()
+	{
+		this.add.image(400, 300, 'sky')
         this.add.image(400, 300, 'star')
-    }
+	}
 }
