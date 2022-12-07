fcb2ae36ac202e91c0d04f8f4a9195a02746d80f
HENRY VERISSIMO <verissimohenry04@gmail.com>
2022-12-07 18:16:57 +0100

    Modify gamescene file

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -9,11 +9,20 @@ export default class GameScene extends Phaser.Scene
     
     preload()
     {
+        this.load.image('sky', 'assets/sky.png')
+		this.load.image('ground', 'assets/platform.png')
+		this.load.image('star', 'assets/star.png')
+		this.load.image('bomb', 'assets/bomb.png')
+
+		this.load.spritesheet('dude', 
+			'assets/dude.png',
+			{ frameWidth: 32, frameHeight: 48 }
+		)
 
     }
 
     create()
     {
-        
+
     }
 }
