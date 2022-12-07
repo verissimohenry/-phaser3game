2a0cf01a9b4fc3dd874a047b4195e51a9deabb90
HENRY VERISSIMO <verissimohenry04@gmail.com>
2022-12-07 18:08:56 +0100

    Modify GameScene file

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+export default class GameScene extends Phaser.Scene
+{
+    constructor()
+    {
+        super('game-scene')
+    }
+    
+    preload()
+    {
+
+    }
+
+    create()
+    {
+        
+    }
+}
